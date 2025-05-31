27d6cfe62a8f4ee80bb969a01801c5b9d299acd2
Harsh Pratap Singh <126321849+Harshyyuu@users.noreply.github.com>
2025-05-31 20:30:58 +0530

    Assignment-Object Oriented Programming.cpp

--- Assignment-Object Oriented Programming.cpp
+++ Assignment-Object Oriented Programming.cpp
@@ -1,109 +1,144 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
 class Student {
 private:
     string name;
-    int rollNumber;
+    int rollno;
     float cgpa;
-    vector<string> courses;
+    string courses[10];
+    int courseCount;
 
 public:
     Student() {
         name = "";
-        rollNumber = 0;
+        rollno = 0;
         cgpa = 0.0;
+        courseCount = 0;
     }
 
     Student(string n, int r, float c) {
         name = n;
-        rollNumber = r;
+        rollno = r;
         cgpa = c;
+        courseCount = 0;
     }
 
-    void addCourse(string course) {
-        courses.push_back(course);
+    Student(const Student &s) {
+        name = s.name;
+        rollno = s.rollno;
+        cgpa = s.cgpa;
+        courseCount = s.courseCount;
+        for (int i = 0; i < courseCount; i++)
+            courses[i] = s.courses[i];
+    }
+
+    ~Student() {}
+
+    void addcourse(string course) {
+        if (courseCount < 10) {
+            courses[courseCount] = course;
+            courseCount++;
+        }
     }
 
     void updateCGPA(float newCGPA) {
-        if (newCGPA >= 0.0 && newCGPA <= 4.0) {
+        if (newCGPA >= 0.0 && newCGPA <= 10.0)
             cgpa = newCGPA;
-        } else {
-            cout << "Invalid CGPA. It should be between 0.0 and 4.0." << endl;
-        }
+        else
+            cout << "Invalid CGPA value!" << endl;
     }
 
     void displayInfo() {
-        cout << "Name: " << name << endl;
-        cout << "Roll Number: " << rollNumber << endl;
-        cout << "CGPA: " << cgpa << endl;
-        cout << "Courses: ";
-        for (int i = 0; i < courses.size(); i++) {
+        cout << "Name: " << name << "\nRoll No: " << rollno
+             << "\nCGPA: " << cgpa << "\nCourses: ";
+        for (int i = 0; i < courseCount; i++)
             cout << courses[i] << " ";
-        }
-        cout << endl;
+        cout << "\n";
     }
 
-    int getRollNumber() {
-        return rollNumber;
+    int getRoll() {
+        return rollno;
     }
 };
 
 class StudentManagementSystem {
 private:
-    vector<Student> students;
+    Student students[100];
+    int count;
 
 public:
-    void addStudent(Student student) {
-        students.push_back(student);
+    StudentManagementSystem() {
+        count = 0;
     }
 
-    Student* searchStudent(int rollNumber) {
-        for (int i = 0; i < students.size(); i++) {
-            if (students[i].getRollNumber() == rollNumber) {
-                return &students[i];
+    void addStudent(Student s) {
+        if (count < 100) {
+            students[count] = s;
+            count++;
+        }
+    }
+
+    void searchStudent(int roll) {
+        for (int i = 0; i < count; i++) {
+            if (students[i].getRoll() == roll) {
+                students[i].displayInfo();
+                return;
             }
         }
-        return nullptr;
+        cout << "Student with roll " << roll << " not found.\n";
     }
 
     void displayAllStudents() {
-        for (int i = 0; i < students.size(); i++) {
+        for (int i = 0; i < count; i++) {
             students[i].displayInfo();
-            cout << "------------------------" << endl;
+            cout << "-----------------\n";
         }
     }
 };
 
 int main() {
-    StudentManagementSystem sms;
+    StudentManagementSystem system;
+    int n;
 
-    Student student1("Alice", 101, 3.5);
-    Student student2("Bob", 102, 3.8);
+    cout << "Enter number of students to add: ";
+    cin >> n;
 
-    sms.addStudent(student1);
-    sms.addStudent(student2);
+    for (int i = 0; i < n; i++) {
+        string name;
+        int roll;
+        float cgpa;
+        int courseCount;
+        string course;
 
-    cout << "All Student Records:" << endl;
-    sms.displayAllStudents();
+        cout << "\nEnter name: ";
+        cin >> name;
+        cout << "Enter roll number: ";
+        cin >> roll;
+        cout << "Enter CGPA: ";
+        cin >> cgpa;
 
-    int rollToSearch = 101;
-    Student* foundStudent = sms.searchStudent(rollToSearch);
-    if (foundStudent != nullptr) {
-        cout << "Found Student:" << endl;
-        foundStudent->displayInfo();
-    } else {
-        cout << "Student with roll number " << rollToSearch << " not found." << endl;
+        Student s(name, roll, cgpa);
+
+        cout << "Enter number of courses: ";
+        cin >> courseCount;
+
+        for (int j = 0; j < courseCount; j++) {
+            cout << "Enter course " << j + 1 << ": ";
+            cin >> course;
+            s.addcourse(course);
+        }
+
+        system.addStudent(s);
     }
 
-    if (foundStudent != nullptr) {
-        foundStudent->updateCGPA(3.9);
-        cout << "Updated Student Information:" << endl;
-        foundStudent->displayInfo();
-    }
+    cout << "\nAll Students:\n";
+    system.displayAllStudents();
+
+    int searchRoll;
+    cout << "\nEnter roll number to search: ";
+    cin >> searchRoll;
+    system.searchStudent(searchRoll);
 
     return 0;
 }
